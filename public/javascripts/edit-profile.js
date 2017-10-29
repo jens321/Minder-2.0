6329d80a91f31aab2666c10416eac46284b354a5
Jens Tuyls <you@example.com>
2017-10-28 18:02:45 -0700

    tag implementation finished using event delegation

--- public/javascripts/edit-profile.js
+++ public/javascripts/edit-profile.js
@@ -10,3 +10,18 @@ $('#save-button').click(function (event) {
     $('.view-card').removeClass('hidden');
     event.preventDefault(); 
 });
+
+// Add tags in edit mode
+$('#tag-input').keyup(function (event) {
+    if (event.keyCode == 32) {
+        var tagDelete = $('<span>').addClass('tag-delete').text('x');
+        var tag = $('<span>').addClass('tag').text($(this).val() + ' ').append(tagDelete); 
+        $('.taglist').append(tag); 
+        $(this).val(''); 
+    }
+});
+
+// use event delegation to delete tags
+$('.taglist').on('click', '.tag-delete', function () {
+    $(this).parent().remove(); 
+})
