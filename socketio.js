18d680cec4e85497726772c89520b3339e9c3bb1
Jens Tuyls <you@example.com>
2017-11-13 00:00:21 -0800

    refactored header and footer into separate ejs files

---
+++ socketio.js
@@ -0,0 +1,7 @@
+// use separte chat route
+// use list groups
+// make chat model 
+// date: { type: Date, default: Date.now()}
+// sender, recipient, message 
+// use data-id html attribute 
+// use hash with identifier to create unique id
